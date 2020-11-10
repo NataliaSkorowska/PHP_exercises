b85528d6c96f072fadec51c778d30e5a1358345e
Natalia Skórowska <nataliaskorowska@gmail.com>
2020-11-10 20:04:54 +0100

    Add files via upload

---
+++ operatory.php
@@ -0,0 +1,16 @@
+<?php
+
+$x = 2;
+$y  = 5.5;
+$z = 14;
+
+$wynik1 = $x ** 2 + $z;
+$wynik2 = $z % $x;
+$wynik3 = ($x + $y);
+
+echo $wynik1 . "<br>";
+echo $wynik2 . "<br>";
+echo $wynik3 . "<br>";
+
+
+?>
