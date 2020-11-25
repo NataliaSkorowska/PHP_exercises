fb8aece7e0fb3c782603d33a7176bba01413483c
Natalia Skórowska <nataliaskorowska@gmail.com>
2020-11-25 15:35:46 +0100

    Create index.php

---
+++ piekarnia/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piekarnia Gdańśka</title>
+</head>
+<br>
+    <h1>Zamówienie online</h1>
+    <form action = "order.php" method = "POST">
+    Ile pączków (0.99 PLN/szt):
+    <input type = 'text' name = 'paczkow'>
+<br>
+<br>
+Ile grzebienie (1.29  PLN/szt):
+<input type = 'text'name = 'grzebieni'>
+<br>
+<br>
+<button type = 'submit' value = "Wyślij zamówienie">Wyślij zamówienie</button>
+</form>  
+</body>
+</html>
