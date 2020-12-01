6aafed79dbb1e07ac062a52a3a72078f1277f76b
Natalia Skórowska <nataliaskorowska@gmail.com>
2020-12-01 15:56:21 +0100

    Add files via upload

---
+++ databases/src/insert.php
@@ -0,0 +1,19 @@
+<?php
+$wartoscZapytania = $_POST['wartosc'];
+$hostname='127.0.0.1';
+$username='root';
+$password='';
+$databasename='phpdb';
+
+$connect = new mysqli
+(
+    $hostname,
+    $username,
+    $password,
+    $databasename);
+    $queryDoZapisuDanych="INSERT INTO wartosci (wartosc) VALUE('" . $wartoscZapytania . "')";
+$result = $connect-> query($queryDoZapisuDanych);
+echo $result;
+
+header("Location:" . $_SERVER['HTTP_REFERER']);
+?>
